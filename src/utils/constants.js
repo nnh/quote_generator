a8b0b3fe3f257a2864364c9a6b2601c69299d2d8
MarikoOhtsuka <mariko.ohtsuka@nnh.go.jp>
2026-01-30 10:07:30 +0900

    Fix research cooperation fee logic and update test scripts

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -11,10 +11,48 @@ const TRIAL_FIELDS = {
 
 const CDISC_ADDITION = 3;
 
-// itemsシート関連
-const ITEMS_FIELDS = {
-  COST_OF_COOPERATION: "研究協力費、負担軽減費",
-};
-
 // 日付フォーマット
 const DATE_FORMAT = "YYYY/MM/DD";
+
+// quotation request sheet
+const QUOTATION_REQUEST_SHEET = {
+  NAME: "Quotation Request",
+  HEADER_ROW: 1,
+  START_ROW: 2,
+  ITEMNAMES: {
+    INSURANCE_FEE: "保険料",
+    RESEARCH_SUPPORT_FEE: "研究協力費、負担軽減費",
+    PREPARE_FEE: "試験開始準備費用",
+    REGISTRATION_FEE: "症例登録毎の支払",
+    REPORT_FEE: "症例最終報告書提出毎の支払",
+  },
+};
+
+// items sheet
+const ITEMS_SHEET = {
+  NAME: "items",
+  HEADER_ROW: 1,
+
+  COLUMNS: {
+    PRICE: "C",
+    UNIT: "D",
+    BASE_UNIT_PRICE: "S",
+  },
+  ROWS: {
+    PREPARE_FEE: 64,
+    REGISTRATION_FEE: 65,
+    REPORT_FEE: 66,
+    INSURANCE_FEE: 78,
+  },
+  ITEMNAMES: {
+    PREPARE_FEE: "試験開始準備費用",
+    REGISTRATION_FEE: "症例登録",
+    REPORT_FEE: "症例報告",
+    INSURANCE_FEE: "保険料",
+  },
+  UNITS: {
+    PER_CASE: "件",
+    PER_FACILITY: "施設",
+    PER_CASE: "症例",
+  },
+};
